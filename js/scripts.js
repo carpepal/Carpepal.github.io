b431ddfaa480900922128d57f2efcd5bae7e11bd
carpepal <carpepal2@gmail.com>
2021-09-25 00:55:10 +0200

    cambio a repositorio version: 1.0

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function display(id) {
+    var documento = document.getElementById(id);
+    if (documento.style.display === "none") {
+        documento.style.display = "block";
+    } else {
+        documento.style.display = "none";
+    }
+}
+
